2896064b49a368cde91e39eba93dcba688a9a295
Sanchit Bajaj <sanchit02@outlook.com>
2021-03-24 00:18:06 +0530

    update box file

--- Components/Box.js
+++ Components/Box.js
@@ -3,25 +3,28 @@ import boxStyles from "../styles/Box.module.css";
 import { Container, ButtonToggle } from "reactstrap";
 import "font-awesome/css/font-awesome.css";
 
+const nextBtn = () => {
+  alert("Next button clicked");
+};
+
 const Box = () => {
   return (
     <Container className={boxStyles.centreBox}>
-      <h2 className={boxStyles.headTwo}>Ready for the quiz</h2>
-      <h4 className={boxStyles.headTwo}>
-        Make sure to watch all of the videos above before starting!
-      </h4>
+      <div className={boxStyles.centerHeading}>
+        <h2>Ready for the quiz</h2>
+        <h4>These question will help you choose the path of your life</h4>
+        <h5>No. of questions: 14</h5>
+      </div>
       <ButtonToggle
         size="lg"
         block
         color="warning"
-        className="p-0"
-        style={{ borderTopLeftRadius: "0", borderTopRightRadius: "0" }}
+        className={boxStyles.button}
+        onClick={() => nextBtn()}
       >
         Next
-        <span>
-          <i className="fa fa-arrow-right p-3"></i>
-        </span>
-      </ButtonToggle>{" "}
+        <i className="fa fa-arrow-right p-3"></i>
+      </ButtonToggle>
     </Container>
   );
 };
